2f94c8677b9c7d9efbf4590105e055268a207d7e
Diukarev <diukarev.sergii@gmail.com>
2019-11-12 10:06:49 +0200

    add guar and handling the 404 case (unknown routes)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from "react-router-dom";
+import {Route, NavLink, Switch} from "react-router-dom";
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
 
-    render () {
-      return (
-        <div className="Blog">
-          <header>
-            <nav>
-              <ul>
-                <li><NavLink to="/posts/" exact>Posts</NavLink></li>
-                <li><NavLink to={{
-                  pathname: '/new-post',
-                  hash: '#submit',
-                  search: '?quick-submit=true'
-                }}>New Post</NavLink></li>
-              </ul>
-            </nav>
-          </header>
-          <Switch>
-            <Route  path="/new-post" component={NewPost}/>
-            <Route  path="/posts" component={Posts}/>
-          </Switch>
-        </div>
-      );
-    }
+  render () {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink to="/posts/" exact>Posts</NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          {this.state.auth ? <Route  path="/new-post" component={NewPost}/> : null}
+          <Route  path="/posts" component={Posts}/>
+          <Route render={() => <h1>Not found</h1>}/>
+          {/*<Redirect from='/' to='/posts' />*/}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
